fc53e7b97011becd54483ba51e19c74880bad760
ketan <ketansondarva2@gmail.com>
2024-02-29 20:53:29 +0530

    Array Enum Tuple

--- Exercise/script.js
+++ Exercise/script.js
@@ -11,3 +11,40 @@ if (pAvailable)
 var pPrice = document.getElementById("pPrice");
 if (pPrice)
     pPrice.innerText += productPrice;
+var ColorPrice;
+(function (ColorPrice) {
+    ColorPrice[ColorPrice["gp"] = 699] = "gp";
+    ColorPrice[ColorPrice["pg"] = 650] = "pg";
+    ColorPrice[ColorPrice["st"] = 712] = "st";
+})(ColorPrice || (ColorPrice = {}));
+;
+var GoldPlatinum = document.getElementById("gold");
+var PinkGold = document.getElementById("pinkgold");
+var SilverTitanium = document.getElementById("silver");
+if (GoldPlatinum)
+    GoldPlatinum.innerText += ColorPrice.gp;
+if (PinkGold)
+    PinkGold.innerText += ColorPrice.pg;
+if (SilverTitanium)
+    SilverTitanium.innerText += ColorPrice.st;
+var products = [
+    { pId: 432, productName: "Samsung Galaxy Note 7", productPrice: 699, productAvailable: true, imageUrl: "SamsungGalaxy_Gold.jpg",
+        productDescription: "Samsung Galaxy Note 7 is a stylish mobile you can ever have. It has 64GB memory." },
+    { pId: 433, productName: "Samsung Galaxy S6 Edge", productPrice: 630, productAvailable: true, imageUrl: "SamsungGalaxy_Gold.jpg",
+        productDescription: "Samsung Galaxy S6 Edge is a stylish mobile you can ever have. It has 64GB memory." },
+    { pId: 434, productName: "Nokia Lumia 640XL", productPrice: 223, productAvailable: false, imageUrl: "SamsungGalaxy_Gold.jpg",
+        productDescription: "Nokia Lumia 640XL is a stylish mobile you can ever have. It has 64GB memory." }
+];
+function getMobileDetails(mobileName, mobileId) {
+}
+for (var i = 0; i < 3; i++) {
+    var mobileName = document.getElementById("pName" + i);
+    var mobilePrice = document.getElementById("pPrice" + i);
+    var availability = document.getElementById("pAvailable" + i);
+    if (mobileName)
+        mobileName.innerText += products[i].productName;
+    if (mobilePrice)
+        mobilePrice.innerText += products[i].productPrice;
+    if (availability)
+        availability.innerText += products[i].productAvailable ? "Available" : "Out Of Stock";
+}
--- Exercise/script.ts
+++ Exercise/script.ts
@@ -13,5 +13,40 @@ if(pAvailable) pAvailable.innerText = productStatus;
 const pPrice =  document.getElementById("pPrice");
 if(pPrice) pPrice.innerText += productPrice;
 
+enum ColorPrice {gp = 699, pg = 650, st = 712};
+
+const GoldPlatinum = document.getElementById("gold");
+const PinkGold = document.getElementById("pinkgold");
+const SilverTitanium = document.getElementById("silver");
+
+if(GoldPlatinum) GoldPlatinum.innerText += ColorPrice.gp;
+if(PinkGold) PinkGold.innerText += ColorPrice.pg;
+if(SilverTitanium) SilverTitanium.innerText += ColorPrice.st;
 
 
+const products: any [] = [
+    {pId:432,productName:"Samsung Galaxy Note 7",productPrice:699,productAvailable:true,imageUrl:"SamsungGalaxy_Gold.jpg",
+    productDescription:"Samsung Galaxy Note 7 is a stylish mobile you can ever have. It has 64GB memory."},
+
+    {pId:433,productName:"Samsung Galaxy S6 Edge",productPrice:630,productAvailable:true,imageUrl:"SamsungGalaxy_Gold.jpg",
+    productDescription:"Samsung Galaxy S6 Edge is a stylish mobile you can ever have. It has 64GB memory."},
+
+    {pId:434,productName:"Nokia Lumia 640XL",productPrice:223,productAvailable:false,imageUrl:"SamsungGalaxy_Gold.jpg",
+    productDescription:"Nokia Lumia 640XL is a stylish mobile you can ever have. It has 64GB memory."}
+];
+
+
+for(let i = 0; i < 3; i++) {
+    const mobileName = document.getElementById("pName"+i);
+    const mobilePrice = document.getElementById("pPrice"+i);
+    const availability = document.getElementById("pAvailable"+i);
+
+    if(mobileName) mobileName.innerText += products[i].productName;
+    if(mobilePrice) mobilePrice.innerText += products[i].productPrice;
+    if(availability) availability.innerText += products[i].productAvailable ? "Available" : "Out Of Stock";
+}
+
+
+function getMobileDetails(mobileName: string, mobileId: number): any {
+
+}
