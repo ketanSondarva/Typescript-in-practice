d1f0a8cd430f1b95e6e7181f99cacf2a90206ec7
ketan <ketansondarva2@gmail.com>
2024-03-08 23:20:59 +0530

    Exercise: used class concept in our project - script.ts

--- Exercise/script.js
+++ Exercise/script.js
@@ -27,24 +27,68 @@ if (PinkGold)
     PinkGold.innerText += ColorPrice.pg;
 if (SilverTitanium)
     SilverTitanium.innerText += ColorPrice.st;
-var products = [
-    { pId: 432, productName: "Samsung Galaxy Note 7", productPrice: 699, productAvailable: true, imageUrl: "SamsungGalaxy_Gold.jpg",
-        productDescription: "Samsung Galaxy Note 7 is a stylish mobile you can ever have. It has 64GB memory." },
-    { pId: 433, productName: "Samsung Galaxy S6 Edge", productPrice: 630, productAvailable: true, imageUrl: "SamsungGalaxy_Gold.jpg",
-        productDescription: "Samsung Galaxy S6 Edge is a stylish mobile you can ever have. It has 64GB memory." },
-    { pId: 434, productName: "Nokia Lumia 640XL", productPrice: 223, productAvailable: false, imageUrl: "SamsungGalaxy_Gold.jpg",
-        productDescription: "Nokia Lumia 640XL is a stylish mobile you can ever have. It has 64GB memory." }
-];
-function getMobileDetails(mobileName, mobileId) {
-}
+// before learning the interface concepts:
+// the type of array will be 'any'
+// const products: any [] = [
+//     {pId:432,productName:"Samsung Galaxy Note 7",productPrice:699,productAvailable:true,imageUrl:"SamsungGalaxy_Gold.jpg",
+//     productDescription:"Samsung Galaxy Note 7 is a stylish mobile you can ever have. It has 64GB memory."},
+//     {pId:433,productName:"Samsung Galaxy S6 Edge",productPrice:630,productAvailable:true,imageUrl:"SamsungGalaxy_Gold.jpg",
+//     productDescription:"Samsung Galaxy S6 Edge is a stylish mobile you can ever have. It has 64GB memory."},
+//     {pId:434,productName:"Nokia Lumia 640XL",productPrice:223,productAvailable:false,imageUrl:"SamsungGalaxy_Gold.jpg",
+//     productDescription:"Nokia Lumia 640XL is a stylish mobile you can ever have. It has 64GB memory."}
+// ];
+// // after learning the interface concepts:
+// interface product {
+//     pId: number;
+//     productName: string;
+//     productPrice: number;
+//     productAvailable: boolean;
+//     imageUrl: string;
+//     productDescription: string;
+// }
+// // type of array will be product(interface)
+// const products: product [] = [
+//     {pId:432,productName:"Samsung Galaxy Note 7",productPrice:699,productAvailable:true,imageUrl:"SamsungGalaxy_Gold.jpg",
+//     productDescription:"Samsung Galaxy Note 7 is a stylish mobile you can ever have. It has 64GB memory."},
+//     {pId:433,productName:"Samsung Galaxy S6 Edge",productPrice:630,productAvailable:true,imageUrl:"SamsungGalaxy_Gold.jpg",
+//     productDescription:"Samsung Galaxy S6 Edge is a stylish mobile you can ever have. It has 64GB memory."},
+//     {pId:434,productName:"Nokia Lumia 640XL",productPrice:223,productAvailable:false,imageUrl:"SamsungGalaxy_Gold.jpg",
+//     productDescription:"Nokia Lumia 640XL is a stylish mobile you can ever have. It has 64GB memory."}
+// ];
+// for(let i = 0; i < 3; i++) {
+//     const mobileName = document.getElementById("pName"+i);
+//     const mobilePrice = document.getElementById("pPrice"+i);
+//     const availability = document.getElementById("pAvailable"+i);
+//     if(mobileName) mobileName.innerText += products[i].productName;
+//     if(mobilePrice) mobilePrice.innerText += products[i].productPrice;
+//     if(availability) availability.innerText += products[i].productAvailable ? "Available" : "Out Of Stock";
+// }
+// above same implementation using the class concepts:
+var $products = /** @class */ (function () {
+    function $products(pId, productName, productPrice, productAvailable, imageUrl, productDescription) {
+        this.pId = pId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productAvailable = productAvailable;
+        this.imageUrl = imageUrl;
+        this.productDescription = productDescription;
+    }
+    return $products;
+}());
+var prod1 = new $products(432, "Samsung Galaxy Note 7", 699, true, "SamsungGalaxy_Gold.jpg", "Samsung Galaxy Note 7 is a stylish mobile you can ever have. It has 64GB memory.");
+var prod2 = new $products(433, "Samsung Galaxy S6 Edge", 630, true, "SamsungGalaxy_Gold.jpg", "Samsung Galaxy S6 Edge is a stylish mobile you can ever have. It has 64GB memory.");
+var prod3 = new $products(434, "Nokia Lumia 640XL", 223, false, "SamsungGalaxy_Gold.jpg", "Nokia Lumia 640XL is a stylish mobile you can ever have. It has 64GB memory.");
+var $prods = [prod1, prod2, prod3];
 for (var i = 0; i < 3; i++) {
     var mobileName = document.getElementById("pName" + i);
     var mobilePrice = document.getElementById("pPrice" + i);
     var availability = document.getElementById("pAvailable" + i);
     if (mobileName)
-        mobileName.innerText += products[i].productName;
+        mobileName.innerText += $prods[i].productName;
     if (mobilePrice)
-        mobilePrice.innerText += products[i].productPrice;
+        mobilePrice.innerText += $prods[i].productPrice;
     if (availability)
-        availability.innerText += products[i].productAvailable ? "Available" : "Out Of Stock";
+        availability.innerText += $prods[i].productAvailable ? "Available" : "Out Of Stock";
+}
+function getMobileDetails(mobileName, mobileId) {
 }
--- Exercise/script.ts
+++ Exercise/script.ts
@@ -39,7 +39,7 @@ if(SilverTitanium) SilverTitanium.innerText += ColorPrice.st;
 
 
 
-// after learning the interface concepts:
+// // after learning the interface concepts:
 interface product {
     pId: number;
     productName: string;
@@ -49,30 +49,62 @@ interface product {
     productDescription: string;
 }
 
-// type of array will be product(interface)
-const products: product [] = [
-    {pId:432,productName:"Samsung Galaxy Note 7",productPrice:699,productAvailable:true,imageUrl:"SamsungGalaxy_Gold.jpg",
-    productDescription:"Samsung Galaxy Note 7 is a stylish mobile you can ever have. It has 64GB memory."},
+// // type of array will be product(interface)
+// const products: product [] = [
+//     {pId:432,productName:"Samsung Galaxy Note 7",productPrice:699,productAvailable:true,imageUrl:"SamsungGalaxy_Gold.jpg",
+//     productDescription:"Samsung Galaxy Note 7 is a stylish mobile you can ever have. It has 64GB memory."},
 
-    {pId:433,productName:"Samsung Galaxy S6 Edge",productPrice:630,productAvailable:true,imageUrl:"SamsungGalaxy_Gold.jpg",
-    productDescription:"Samsung Galaxy S6 Edge is a stylish mobile you can ever have. It has 64GB memory."},
+//     {pId:433,productName:"Samsung Galaxy S6 Edge",productPrice:630,productAvailable:true,imageUrl:"SamsungGalaxy_Gold.jpg",
+//     productDescription:"Samsung Galaxy S6 Edge is a stylish mobile you can ever have. It has 64GB memory."},
 
-    {pId:434,productName:"Nokia Lumia 640XL",productPrice:223,productAvailable:false,imageUrl:"SamsungGalaxy_Gold.jpg",
-    productDescription:"Nokia Lumia 640XL is a stylish mobile you can ever have. It has 64GB memory."}
-];
+//     {pId:434,productName:"Nokia Lumia 640XL",productPrice:223,productAvailable:false,imageUrl:"SamsungGalaxy_Gold.jpg",
+//     productDescription:"Nokia Lumia 640XL is a stylish mobile you can ever have. It has 64GB memory."}
+// ];
 
+// for(let i = 0; i < 3; i++) {
+//     const mobileName = document.getElementById("pName"+i);
+//     const mobilePrice = document.getElementById("pPrice"+i);
+//     const availability = document.getElementById("pAvailable"+i);
+
+//     if(mobileName) mobileName.innerText += products[i].productName;
+//     if(mobilePrice) mobilePrice.innerText += products[i].productPrice;
+//     if(availability) availability.innerText += products[i].productAvailable ? "Available" : "Out Of Stock";
+// }
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// above same implementation using the class concepts:
+class $products { // used '$' because class 'products' already defined
+    pId: number;
+    productName: string;
+    productPrice: number;
+    productAvailable: boolean;
+    imageUrl: string;
+    productDescription: string;
+
+    constructor(pId: number, productName: string, productPrice: number, productAvailable: boolean, imageUrl: string, productDescription: string) {
+        this.pId = pId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productAvailable = productAvailable;
+        this.imageUrl = imageUrl;
+        this.productDescription = productDescription;
+    }
+} // note: you can also implement the 'product' interface(declared above) in this class !!
+
+const prod1 = new $products(432,"Samsung Galaxy Note 7",699,true,"SamsungGalaxy_Gold.jpg","Samsung Galaxy Note 7 is a stylish mobile you can ever have. It has 64GB memory.");
+const prod2 = new $products(433,"Samsung Galaxy S6 Edge",630,true,"SamsungGalaxy_Gold.jpg","Samsung Galaxy S6 Edge is a stylish mobile you can ever have. It has 64GB memory.");
+const prod3 = new $products(434,"Nokia Lumia 640XL",223,false,"SamsungGalaxy_Gold.jpg","Nokia Lumia 640XL is a stylish mobile you can ever have. It has 64GB memory.");
+
+
+const $prods : $products [] = [ prod1,prod2,prod3 ];
 
 for(let i = 0; i < 3; i++) {
     const mobileName = document.getElementById("pName"+i);
     const mobilePrice = document.getElementById("pPrice"+i);
     const availability = document.getElementById("pAvailable"+i);
 
-    if(mobileName) mobileName.innerText += products[i].productName;
-    if(mobilePrice) mobilePrice.innerText += products[i].productPrice;
-    if(availability) availability.innerText += products[i].productAvailable ? "Available" : "Out Of Stock";
-}
-
-
-function getMobileDetails(mobileName: string, mobileId: number): any {
-
+    if(mobileName) mobileName.innerText += $prods[i].productName;
+    if(mobilePrice) mobilePrice.innerText += $prods[i].productPrice;
+    if(availability) availability.innerText += $prods[i].productAvailable ? "Available" : "Out Of Stock";
 }
